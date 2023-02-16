1afb552eced61bd051c3e2a83913dd6d4daecaeb
a m <113930724+Amoreno73@users.noreply.github.com>
2023-02-16 18:40:41 +0000

    learned how to manage text files

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 # remember to fork this and push to github
 
+# f = open('employees.txt','a')
+# f.write("angel - lead manager")
+# f.write("luis - lead manager")
+# #read gives us everything 
+# #readline gives us the first line
+# print(f.read())
+# f.close()
+
+#context manager
+# with open("text.txt", "r") as f:
+#   # f_contents = f.read()
+#   # f.write("bgkgfbdjkbgjbabjvbdbsbsgjb")
+#   # print(f)
+#   for line in f:
+#     print(line, end='')
+
+with open('period7.txt','a') as f:
+  f.write(" second content appended")
+  
+    
+
+
